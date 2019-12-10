b939c8732a72d354839d27404c8e564cd066b84b
YaddaShang <58722820+YaddaShang@users.noreply.github.com>
2019-12-10 19:12:45 +0800

    Add files via upload

---
+++ scripts/train_resnet_256.sh
@@ -0,0 +1,9 @@
+TRAIN_SET=/home/user261/shangyue/preprocess_dataset
+python train.py $TRAIN_SET \
+--dispnet DispResNet \
+--num-scales 1 \
+-b4 -s0.1 -c0.5 --epoch-size 1000 --sequence-length 3 \
+--with-mask \
+--with-ssim \
+--with-gt \
+--name resnet_256
